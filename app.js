8c48dbd418d6fd24fc7f9bd6306e798ca023c2d6
RakibRahman <md.rr.talukder@gmail.com>
2021-06-07 22:02:26 +0600

    Animation added

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const dice = document.getElementById("dice");
 const roller = document.getElementById("roller");
+const btn = document.getElementById("btn");
 
-function rollDice() {
+function randomNumber() {
   const numbers = [1, 2, 3, 4, 5, 6];
   numbers.sort((a, b) => 0.5 - Math.random());
-  roller.src = `img/${numbers[0]}.png`;
+  return numbers[0];
 }
-rollDice();
+function onLoading() {
+  roller.src = `img/${randomNumber()}.png`;
+}
+function rollDice() {
+  let rollerSrc = `img/${randomNumber()}.png`;
+  const img = document.createElement("img");
+  img.src = rollerSrc;
+  dice.innerHTML = "";
+  dice.append(img);
+  img.classList.add("animate__animated", "animate__jackInTheBox");
+}
+
+function init() {
+  btn.addEventListener("click", rollDice);
+  onLoading();
+}
+init();
